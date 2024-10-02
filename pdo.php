7b8b2d9a7d7f0aedd631a4d5f1c23f3762bde974
jeremy.kang'ethe@strathmore.edu <jeremy.kangethe@gmail.com>
2024-10-02 15:44:45 +0300

    firstcommit

---
+++ pdo.php
@@ -0,0 +1,17 @@
+<?php
+//Creating constants
+  $servername = "localhost:3307";
+  $username = "root";
+  $password = "";
+  $dbname = "iap";
+
+  try {
+    // Create the connection
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully :-)";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+?> 
