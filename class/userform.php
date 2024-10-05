ef2d9eebffe9aa7dabfde8993a4d6ad00bf61445
jeremy.kang'ethe@strathmore.edu <jeremy.kangethe@gmail.com>
2024-10-05 20:33:29 +0300

    display final

--- class/userform.php
+++ class/userform.php
@@ -44,7 +44,7 @@ function sign_up(){
     </div>
   </div>
   <button type="submit" class="btn btn-primary">Sign in</button>
-  <button type="submit" class="btn btn-primary" href="">View Users</button>
+  <a href="display.php" class="btn btn-primary">View Users</a>
 </form>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
